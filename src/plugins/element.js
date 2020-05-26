39e5bbe66a49f595648cd00d2816962d361e7b2c
415172198 <415172198@qq.com>
2020-05-26 16:05:07 +0800

    完成了登陆功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,6 +5,5 @@ import { Button,Form,FormItem,Input,Message } from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
-Vue.use(Input)git
+Vue.use(Input)
 Vue.prototype.$message = Message
-
